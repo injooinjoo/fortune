a62a89fed1afac6e8e1730248449ab672dc2afcc
INJOO <injooinjoo@gmail.com>
2026-03-10 02:29:24 +0900

    [KAN-52] chore: prune additional doc-only legacy clusters

--- lib/presentation/widgets/profile_edit_dialogs/index.dart
+++
@@ -1,5 +0,0 @@
-export 'profile_field_edit_dialog.dart';
-export 'birth_date_edit_dialog.dart';
-export 'birth_time_edit_dialog.dart';
-export 'blood_type_edit_dialog.dart';
-export 'mbti_edit_dialog.dart';
